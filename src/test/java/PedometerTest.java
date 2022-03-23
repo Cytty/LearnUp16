71a4fc4abf24f3f9356ccae80a0d8034fded5429
Cytty <98545084+Cytty@users.noreply.github.com>
2022-03-23 14:10:42 +0300

    Update PedometerTest.java

--- src/test/java/PedometerTest.java
+++ src/test/java/PedometerTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import java.util.HashMap;
 
 public class PedometerTest {
@@ -22,13 +23,27 @@ public class PedometerTest {
         pedometer.add(3, 900);
         pedometer.add(3, 400);
         pedometer.add(4, 2_000);
-        pedometer.add(5, 3_000);
 
         int expected = 3_901;
-        int actual = pedometer.betterDay(steps);
+        int actual = pedometer.add(5, 3000);
 
         Assertions.assertEquals(expected, actual);
-
     }
 
+    @Test
+    public void allTest0() {
+        HashMap<Integer, Integer> steps = new HashMap<>();
+        Pedometer pedometer = new Pedometer(steps);
+        pedometer.add(1, 2_000);
+        pedometer.add(1, 1_500);
+        pedometer.add(2, -1_400);
+        pedometer.add(2, -1_000);
+        pedometer.add(2, 2_400);
+        pedometer.add(2, 2_000);
+
+        int expected = 1;
+        int actual = pedometer.add(2, 300);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
