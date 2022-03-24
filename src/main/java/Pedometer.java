281925eff01c0c33dfe18d67ba33751928501ac5
Cytty <cytty42@gmail.com>
2022-03-24 09:39:30 +0300

    Final commit

--- src/main/java/Pedometer.java
+++ src/main/java/Pedometer.java
@@ -9,26 +9,24 @@ public class Pedometer {
 
     public int add(int day, int step) {
         int sumStep;
-        if (step <= 0) {
-            sumStep = steps.getOrDefault(day, 0);
+        int maxStep;
+        int max = 0;                                           //ДОБАВЛЯЕМ ШАГИ
+        if (step <= 0 || steps.size() > Math.abs(day)) {      //если значение шагов отрицательное или день - прошедший
+            sumStep = steps.getOrDefault(day, 0);    //оставить сумму шагов дня без изменений
         } else {
-            sumStep = steps.getOrDefault(day, 0) + step;
+            sumStep = steps.getOrDefault(Math.abs(day), 0) + step; //иначе  добавь шаги к имеющемся в этом дне
         }
-        steps.put(day, sumStep);
 
-        int max = 0;
-        int maxStep = 0;
-        for (int title : steps.keySet()) {
-            if (steps.get(title) > max) {
-                max = steps.get(title);
-            }
-        }
-        for (int title : steps.keySet()) {
-            if (!steps.containsKey(day + 1)) {
-                maxStep = max - steps.get(title) + 1;
-            } else {
-                continue;
-            }
+        steps.put(day, sumStep);                                //ИЩЕМ ЛУЧШИЙ ДЕНЬ
+        if (steps.size() > Math.abs(day)) {                     //если добавляемый день уже прошел
+            maxStep = -1;                                       // вернуть "-1"
+        } else {
+            for (int title : steps.keySet()) {                    //каждое значение в мапе
+                if (steps.get(title) > max) {                     //сравниваем с максимумом, и если значение дня больше,
+                    max = steps.get(title);                       //то теперь это значение является максимумом
+                }
+            }                                                    //РАСЧИТЫВАЕМ СКОЛЬКО ШАГОВ ОСТАЛОСЬ ДО ПОБИТИЯ РЕКОРДА
+            maxStep = max - steps.get(day) + 1;                  //вычитаем из максимума пройденные шаги и прибавляем 1
         }
         return maxStep;
     }
--- src/test/java/PedometerTest.java
+++ src/test/java/PedometerTest.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 public class PedometerTest {
 
     @Test
-    public void allTest() {
+    public void negativeVal() {
         HashMap<Integer, Integer> steps = new HashMap<>();
         Pedometer pedometer = new Pedometer(steps);
         pedometer.add(1, 2_000);
@@ -31,7 +31,7 @@ public class PedometerTest {
     }
 
     @Test
-    public void allTest0() {
+    public void todayBestDay() {
         HashMap<Integer, Integer> steps = new HashMap<>();
         Pedometer pedometer = new Pedometer(steps);
         pedometer.add(1, 2_000);
@@ -43,6 +43,56 @@ public class PedometerTest {
 
         int expected = 1;
         int actual = pedometer.add(2, 300);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void lastDay() {
+        HashMap<Integer, Integer> steps = new HashMap<>();
+        Pedometer pedometer = new Pedometer(steps);
+        pedometer.add(1, 2_000);
+        pedometer.add(1, 1_500);
+        pedometer.add(2, -1_400);
+        pedometer.add(2, -1_000);
+        pedometer.add(2, 2_400);
+        pedometer.add(2, 2_000);
+        pedometer.add(2, 600);
+        pedometer.add(2, 200);
+        pedometer.add(2, 900);
+        pedometer.add(2, 500);
+        pedometer.add(2, 300);
+        pedometer.add(3, 900);
+        pedometer.add(3, 1400);
+        pedometer.add(4, 2_000);
+
+        int expected = -1;
+        int actual = pedometer.add(2, 300);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void negativeDay() {
+        HashMap<Integer, Integer> steps = new HashMap<>();
+        Pedometer pedometer = new Pedometer(steps);
+        pedometer.add(1, 2_000);
+        pedometer.add(1, 1_500);
+        pedometer.add(2, -1_400);
+        pedometer.add(2, -1_000);
+        pedometer.add(2, 2_400);
+        pedometer.add(2, 2_000);
+        pedometer.add(2, 600);
+        pedometer.add(2, 200);
+        pedometer.add(2, 900);
+        pedometer.add(2, 500);
+        pedometer.add(2, 300);
+        pedometer.add(3, 900);
+        pedometer.add(3, 1400);
+        pedometer.add(4, 2_000);
+
+        int expected = 3_901;
+        int actual = pedometer.add(-5, 3000);
 
         Assertions.assertEquals(expected, actual);
     }
